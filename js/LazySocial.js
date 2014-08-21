521bc2d9c046bbbbe97f06abb6494c26515f1762
mrcode86 <mrcode86@gmail.com>
2014-08-21 11:16:12 +0200

    Signed-off-by: mrcode86

--- js/LazySocial.js
+++ js/LazySocial.js
@@ -43,12 +43,24 @@
         var Url = activeList.attr("data-href");
         var Facebook = activeList.find(".LazyFacebook:first");
         var Google = activeList.find(".LazyGoogle:first");
+        var Twitter = activeList.find(".LazyTwitter:first");
+        var Pintrest = activeList.find(".LazyPintrest:first");
+        var LinkedIn = activeList.find(".LazyLinkedIn:first");
 
         if (Facebook.length) {
             LazyFacebook.CreateButton(Facebook, Url);
         }
         if (Google.length) {
             LazyGoogle.CreateButton(Google, Url);
+        }
+        if (Twitter.length) {
+            LazyTwitter.CreateButton(Twitter, Url);
+        }
+        if (Pintrest.length) {
+            LazyPintrest.CreateButton(Pintrest, Url);
+        }
+        if (LinkedIn.length) {
+            LazyLinkedIn.CreateButton(LinkedIn, Url);
         }
     }
 };
@@ -63,9 +75,10 @@ var LazyFacebook = {
         if (window.FB) {
             callback();
         } else {
-            JavaScript.Load(document.location.protocol + '//connect.facebook.net/' + LazySocial.LanguageIso.replace("-", "_") + '/all.js', function () {
-                callback();
-            });
+            window.jQuery.getScript('//connect.facebook.net/' + LazySocial.LanguageIso.replace("-", "_") + '/all.js')
+                .done(function () {
+                    callback();
+                });
         }
     },
     // Create and load the facebook button
@@ -118,7 +131,7 @@ var LazyGoogle = {
         if (window.gapi && window.gapi.plusone) {
             callback();
         } else {
-            window.jQuery.getScript("https://apis.google.com/js/plusone.js")
+            window.jQuery.getScript("//apis.google.com/js/plusone.js")
                 .done(function () {
                     callback();
                 });
@@ -153,23 +166,46 @@ var LazyGoogle = {
 
 var LazyTwitter = {
     LoadJs: function (callback) {
-        if (window.gapi && window.gapi.plusone) {
+        if (window.twttr) {
             callback();
         } else {
-            window.jQuery.getScript("https://platform.twitter.com/widgets.js")
+            window.jQuery.getScript("//platform.twitter.com/widgets.js")
                 .done(function () {
                     callback();
                 });
         }
     },
     CreateButton: function (item, url) {
+        LazyTwitter.LoadJs(function () {
+            var Id = "Tw_" + LazySocial.GetId();
+            var Button = LazyTwitter.GetHtmlButton(item, url);
+            item = jQuery(item);
+
+            item.removeClass("LazyTwitter");
+            item.html(Button);
+            item.attr("id", Id);
+
+            twttr.widgets.load(document.getElementById(Id));
+        });
     },
     GetHtmlButton: function (item, url) {
         item = jQuery(item);
         var Via = "";
+        var Text = "";
+        var Count = "";
+        var Lang = "";
 
         if (item.is("[data-via]")) {
             Via = item.attr("data-via");
+        }
+        if (item.is("[data-text]")) {
+            Text = item.attr("data-text");
+        }
+        if (item.is("[data-count]")) {
+            Count = item.attr("data-count");
+        }
+        if (item.is("[data-lang]")) {
+            Lang = item.attr("data-lang");
         }
 
         var Btn = "<a href='https://twitter.com/share' class='twitter-share-button'";
@@ -177,6 +213,15 @@ var LazyTwitter = {
 
         if (Via != "") {
             Btn += " data-via='" + Via + "'";
+        }
+        if (Text != "") {
+            Btn += " data-text='" + Text + "'";
+        }
+        if (Count != "") {
+            Btn += " data-count='" + Count + "'";
+        }
+        if (Lang != "") {
+            Btn += " data-lang='" + Lang + "'";
         }
 
         Btn += ">Tweet</a>";
@@ -186,19 +231,66 @@ var LazyTwitter = {
 
 var LazyPintrest = {
     LoadJs: function (callback) {
+        window.jQuery.getScript("//assets.pinterest.com/js/pinit.js")
+            .done(function () {
+                callback();
+            });
     },
     CreateButton: function (item, url) {
+        LazyPintrest.LoadJs(function () {
+            var Id = "Pin_" + LazySocial.GetId();
+            var Button = LazyPintrest.GetHtmlButton(item, url);
+            item = jQuery(item);
+
+            item.removeClass("LazyPintrest");
+            item.html(Button);
+            item.attr("id", Id);
+        });
     },
     GetHtmlButton: function (item, url) {
+        var Url = "//www.pinterest.com/pin/create/button/?url=" + encodeURIComponent(url) + "";
+
+        if (item.is("[data-media]")) {
+            Url += "&media=" + encodeURIComponent(item.attr("data-media"));
+        }
+        if (item.is("[data-description]")) {
+            Url += "&description=" + encodeURIComponent(item.attr("data-description"));
+        }
+
+        var Html = "<a href='" + Url + "' data-pin-do='buttonBookmark'><img src='//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png' /></a>";
+
+        return Html;
     }
 };
 
 var LazyLinkedIn = {
     LoadJs: function (callback) {
+        if (window.IN) {
+            callback();
+        } else {
+            window.jQuery.getScript("//platform.linkedin.com/in.js")
+                .done(function () {
+                    callback();
+                });
+        }
     },
     CreateButton: function (item, url) {
+        LazyLinkedIn.LoadJs(function () {
+            var Id = "Lin_" + LazySocial.GetId();
+            var Button = LazyLinkedIn.GetHtmlButton(item, url);
+            item = jQuery(item);
+
+            item.removeClass("LazyLinkedIn");
+            item.html(Button);
+            item.attr("id", Id);
+            
+            if (IN.parse) {
+                window.IN.parse(document.getElementById(Id));
+            }
+        });
     },
     GetHtmlButton: function (item, url) {
+        return "<script type='IN/Share' data-url='" + url + "' data-counter='right'></script>";
     }
 };
 
